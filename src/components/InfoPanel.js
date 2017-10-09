976438f0a8fb2e1b97f0fa52d080045c53b8c056
boybutcher <BYRONdeLARGE@yahoo.com>
2017-10-08 17:02:33 -0700

    refactored modal

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -9,20 +9,41 @@ class InfoPanel extends Component {
         display_name,
         profile_url,
         twitter,
-      } = {},
+      } = {
+        avatar_url: 'https://i.pinimg.com/236x/64/78/b4/6478b40ee5f7f169b6d25f914ccfcac2--silhouette-viking.jpg',
+        display_name: 'Anonymous User',
+        profile_url: null,
+        twitter: null,
+      },
       source_post_url,
       import_datetime,
       bitly_url,
     } = this.props.imageData;
 
-    const standinAvatar = 'https://i.pinimg.com/236x/64/78/b4/6478b40ee5f7f169b6d25f914ccfcac2--silhouette-viking.jpg';
+    const avatarStyle = {
+      backgroundImage: `url('${avatar_url}')`,
+    }
 
     return (
-      <span className='InfoPanel'>
-        <span>
-          
+      <div className='InfoPanel' onClick={(e) => e.stopPropagation()}>
+        <span
+          className='avatar'
+          style={avatarStyle}
+        >
         </span>
-      </span>
+        <span className='post-data'>
+          <a
+            className='poster-name'
+            href={profile_url}
+            target='_blank'
+          > 
+            {display_name} 
+          </a>
+          <sub className='post-date'>
+            posted on {new Date(import_datetime).toString().split(' ').slice(0, 5).join(' ')}
+          </sub>
+        </span>
+      </div>
     )
   }
 };
