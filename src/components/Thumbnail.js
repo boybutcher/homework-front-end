37cd0f7191f26c037d66abd73a39ae77867e5b35
boybutcher <BYRONdeLARGE@yahoo.com>
2017-10-08 17:24:39 -0700

    link styling, onHover creates preview

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import '../styles/Thumbnail.css';
 
 class Thumbnail extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      previewing: false
+    }
+    this.togglePreview = this.togglePreview.bind(this);
+  }
+
+  togglePreview() {
+    this.setState({
+      previewing: !this.state.previewing,
+    })
+  }
+
   render() {
     let {
       gif,
@@ -10,8 +23,10 @@ class Thumbnail extends Component {
       selectImage,
     } = this.props;
 
-    let thumbnailImage = {
+    let thumbnailImage = this.state.previewing ? {
       backgroundImage: `url('${gif.images.preview_gif.url}')`,
+    } : {
+      backgroundImage: `url('${gif.images.original_still.url}')`
     }
 
     return (
@@ -19,6 +34,8 @@ class Thumbnail extends Component {
         className='Thumbnail' 
         style={thumbnailImage}
         onClick={() => selectImage(index)}
+        onMouseEnter={this.togglePreview}
+        onMouseLeave={this.togglePreview}
       >
       </span>
     )
